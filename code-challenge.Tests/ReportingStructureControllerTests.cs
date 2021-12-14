eb8f6b9e3a4436abfa30e520b3f10cc2d7ed4a49
Alex Amari <arl2312@rit.edu>
2021-12-14 17:04:37 -0500

    Stub REST endpoint

---
+++ code-challenge.Tests/ReportingStructureControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using challenge.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using code_challenge.Tests.Integration.Extensions;
+using System.Net;
+using System.Net.Http;
+
+namespace code_challenge.Tests.Integration
+{
+    [TestClass]
+    public class ReportingStructureControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<TestServerStartup>()
+                .UseEnvironment("Development"));
+
+            _httpClient = _testServer.CreateClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [TestMethod]
+        [DataRow("16a596ae-edd3-4847-99fe-c4518e82c86f", "John", "Lennon", 4)]
+        [DataRow("b7839309-3348-463b-a7e3-5de1c168beb3", "Paul", "McCartney", 0)]
+        public void GetReportingStructureByEmployeeId_Returns_Ok(string employeeId, string expectedFirstName, 
+                                                                 string expectedLastName, int expectedNumberOfReports)
+        {
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/reporting-structure/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var reportingStructure = response.DeserializeContent<ReportingStructure>();
+            Assert.AreEqual(expectedFirstName, reportingStructure.Employee.FirstName);
+            Assert.AreEqual(expectedLastName, reportingStructure.Employee.LastName);
+            Assert.AreEqual(expectedNumberOfReports, reportingStructure.NumberOfReports);
+        }
+    }
+}
---
+++ code-challenge/Controllers/ReportingStructureController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace challenge.Controllers
+{
+    [Route("api/reporting-structure")]
+    public class ReportingStructureController
+    {
+        [HttpGet("{id}", Name = "getReportingStructureByEmployeeId")]
+        public IActionResult GetReportingStructureByEmployeeId(String employeeId)
+        {
+            return null;
+        }
+    }
+}
