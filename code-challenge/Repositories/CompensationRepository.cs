72cf9108984e498ad4dcaabdc1f571b54891b1cd
Alex Amari <alexandrealivings@gmail.com>
2021-12-19 17:54:49 -0500

    Stub in a compensation repository

---
+++ code-challenge/Repositories/CompensationRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using challenge.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using challenge.Data;
+
+namespace challenge.Repositories
+{
+    public class CompensationRepository : ICompensationRepository
+    {
+        private readonly EmployeeContext _employeeContext;
+        private readonly ILogger<CompensationRepository> _logger;
+
+        public CompensationRepository(ILogger<CompensationRepository> logger, EmployeeContext employeeContext)
+        {
+            _employeeContext = employeeContext;
+            _logger = logger;
+        }
+
+        public Compensation Add(Compensation compensation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Compensation GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
